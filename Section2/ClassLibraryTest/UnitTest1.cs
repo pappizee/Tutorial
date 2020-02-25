a7d35f9aadf00005b08ce0cb1d67f59eb4f735b0
Titilope Fadipe <titilopefadipe@hotmail.com>
2020-02-25 18:38:04 +0000

    TutorialCommit

---
+++ Section2/ClassLibraryTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int result = ClassLibraryPractice.Class1.Addition(10, 5);
+            Assert.AreEqual(result, 15);
+
+            // Common short cut commands :
+            //Peek Alt f12 lets know what the code does
+            //Cntrl tab to flick through tabs
+            //Cntrl S to save
+            //Back space to undo
+            //dll : dynamic link library
+            // read about character escape sequence
+        }
+    }
+}
